01a93855a10d81a24cc7d8296bdef65cff4d5bc0
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2025-10-01 01:09:41 -0400

    feat: use workersai

--- app/agents/models.ts
+++ app/agents/models.ts
@@ -3,8 +3,8 @@ import type { LanguageModel } from "ai";
 import { openai } from "@ai-sdk/openai";
 
 export const getModel = (env: Env): LanguageModel => {
-  // const workersAi = createWorkersAI({ binding: env.AI });
-  // return workersAi("@cf/openai/gpt-oss-120b" as any, {}); // any to fix weird type error
-  return openai("gpt-4o-mini");
+  const workersAi = createWorkersAI({ binding: env.AI });
+  return workersAi("@cf/openai/gpt-oss-120b" as any, {}); // any to fix weird type error
+  // return openai("gpt-4o-mini");
 }
 
