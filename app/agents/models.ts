622cc45302bccd47855aa69eef887a99d20164b4
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2025-10-01 01:19:21 -0400

    fix: switch to mistral-7b

--- app/agents/models.ts
+++ app/agents/models.ts
@@ -1,10 +1,10 @@
 import { createWorkersAI } from "workers-ai-provider";
 import type { LanguageModel } from "ai";
-import { openai } from "@ai-sdk/openai";
+// import { openai } from "@ai-sdk/openai";
 
 export const getModel = (env: Env): LanguageModel => {
   const workersAi = createWorkersAI({ binding: env.AI });
-  return workersAi("@cf/openai/gpt-oss-120b" as any, {}); // any to fix weird type error
+  return workersAi("@cf/mistral/mistral-7b-instruct-v0.1"); // any to fix weird type error
   // return openai("gpt-4o-mini");
 }
 
